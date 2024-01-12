de5c63ebf0fcfca4b4270567c16e59aa0cc5869d
kingpotato <1002638696@qq.com>
2024-01-12 14:05:05 +0800

    queens passed

--- riscv/src/register.v
+++ riscv/src/register.v
@@ -58,7 +58,7 @@ module register (
     debug <= reg_rename[13];
     debug1 <= reg_value[13];
     debug2 <= reg_value[0];
-    debug3 <= reg_busy[31];
+    debug3 <= reg_busy[9];
     a0 <= reg_value[10];
     a1 <= reg_value[11];
     a2 <= reg_value[12];
@@ -84,63 +84,65 @@ module register (
         for (i = 0; i < 32; ++i) begin
           reg_busy[i] <= 0;
         end
-      end
-      if (register_update_flag) begin
-        if (rename_of_commit_ins == reg_rename[register_commit_dest])
-          reg_busy[register_commit_dest] <= 0;
-        if (register_commit_dest != 0) reg_value[register_commit_dest] <= register_commit_value;
-        else reg_value[0] <= 0;
-      end
-      if (rename_need) begin
-        if (rename_need_ins_is_simple) begin
-          rename_finish <= 0;
-          simple_ins_commit <= 1;
-          simple_ins_rename <= new_ins_rd_rename;
-          reg_busy[new_ins_rd] <= 1;
-          reg_rename[new_ins_rd] <= new_ins_rd_rename;
-        end else begin
-          simple_ins_commit <= 0;
-          rename_finish <= 1;
-          if (operand_1_flag) begin
-            if (reg_busy[operand_1_reg]) begin
-              if(register_update_flag && operand_1_reg == register_commit_dest && rename_of_commit_ins == reg_rename[register_commit_dest]) begin
-                operand_1_busy <= 0;
-                operand_1_data_from_reg <= register_commit_value;
-              end else begin
-                operand_1_busy   <= 1;
-                operand_1_rename <= reg_rename[operand_1_reg];
-              end
-            end else begin
-              operand_1_busy <= 0;
-              operand_1_data_from_reg <= reg_value[operand_1_reg];
-            end
-          end
-          if (operand_2_flag) begin
-            if (reg_busy[operand_2_reg]) begin
-
-              if(register_update_flag && operand_2_reg == register_commit_dest && rename_of_commit_ins == reg_rename[register_commit_dest]) begin
-                operand_2_busy <= 0;
-                operand_2_data_from_reg <= register_commit_value;
-              end else begin
-                operand_2_busy   <= 1;
-                operand_2_rename <= reg_rename[operand_2_reg];
-              end
-            end else begin
-              operand_2_busy <= 0;
-              operand_2_data_from_reg <= reg_value[operand_2_reg];
-            end
-          end
-          if (!rename_need_ins_is_branch_or_store) begin
-            //分支指令那个位置不是rd,不需要重命名
-            reg_busy[new_ins_rd] <= 1;
-            reg_rename [new_ins_rd] <= new_ins_rd_rename;//理论上来讲后赋值会覆盖先赋值,如果出问题可改成特判
-          end
-          rename_finish_id <= rename_need_id;
-        end
       end else begin
-        rename_finish <= 0;
-        simple_ins_commit <= 0;
+        if (register_update_flag) begin
+          if (rename_of_commit_ins == reg_rename[register_commit_dest])
+            reg_busy[register_commit_dest] <= 0;
+          if (register_commit_dest != 0) reg_value[register_commit_dest] <= register_commit_value;
+          else reg_value[0] <= 0;
+        end
+        if (rename_need) begin
+          if (rename_need_ins_is_simple) begin
+            rename_finish <= 0;
+            simple_ins_commit <= 1;
+            simple_ins_rename <= new_ins_rd_rename;
+            reg_busy[new_ins_rd] <= 1;
+            reg_rename[new_ins_rd] <= new_ins_rd_rename;
+          end else begin
+            simple_ins_commit <= 0;
+            rename_finish <= 1;
+            if (operand_1_flag) begin
+              if (reg_busy[operand_1_reg]) begin
+                if(register_update_flag && operand_1_reg == register_commit_dest && rename_of_commit_ins == reg_rename[register_commit_dest]) begin
+                  operand_1_busy <= 0;
+                  operand_1_data_from_reg <= register_commit_value;
+                end else begin
+                  operand_1_busy   <= 1;
+                  operand_1_rename <= reg_rename[operand_1_reg];
+                end
+              end else begin
+                operand_1_busy <= 0;
+                operand_1_data_from_reg <= reg_value[operand_1_reg];
+              end
+            end
+            if (operand_2_flag) begin
+              if (reg_busy[operand_2_reg]) begin
+
+                if(register_update_flag && operand_2_reg == register_commit_dest && rename_of_commit_ins == reg_rename[register_commit_dest]) begin
+                  operand_2_busy <= 0;
+                  operand_2_data_from_reg <= register_commit_value;
+                end else begin
+                  operand_2_busy   <= 1;
+                  operand_2_rename <= reg_rename[operand_2_reg];
+                end
+              end else begin
+                operand_2_busy <= 0;
+                operand_2_data_from_reg <= reg_value[operand_2_reg];
+              end
+            end
+            if (!rename_need_ins_is_branch_or_store) begin
+              //分支指令那个位置不是rd,不需要重命名
+              reg_busy[new_ins_rd] <= 1;
+              reg_rename [new_ins_rd] <= new_ins_rd_rename;//理论上来讲后赋值会覆盖先赋值,如果出问题可改成特判
+            end
+            rename_finish_id <= rename_need_id;
+          end
+        end else begin
+          rename_finish <= 0;
+          simple_ins_commit <= 0;
+        end
       end
+
     end
 
   end
